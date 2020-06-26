a2321bc56d3e425f01bc15c63f542153995feb33
lavanyasankaran <lavan.sankar97@gmail.com>
2020-06-26 19:15:49 +0530

    calculation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = { num1: '', num2: '', total: '' }
+  }
+
+
+  add(){
+    
+    this.setState({ total: parseInt(this.state.num1) + parseInt(this.state.num2) })
+    
+  }
+  sub(){
+    
+    this.setState({ total: this.state.num1 - this.state.num2 })
+    
+  }
+  mul(){
+    
+    this.setState({ total: this.state.num1 * this.state.num2 })
+    
+  }
+  div(){
+    
+    this.setState({ total: this.state.num1 / this.state.num2 })
+    
+  }
+
+  render() {
+    return(
+      <div className="mainContainer">
+        <h1>  Numbers</h1>
+
+        <input type="number" className="inputStyle"  onChange={ (eve) => { this.setState({ num1: eve.target.value }) } }required/>
+        <br/><br/>
+
+        <input type="number" className="inputStyle"  onChange={ (eve) => { this.setState({ num2: eve.target.value })} }required />
+        <br/><br/>
+
+        <button onClick={()=>{this.add()}} >Add </button>
+        <button onClick={()=>{this.sub()}} >sub </button>
+        <button onClick={()=>{this.mul()}} >mul</button>
+        <button onClick={()=>{this.div()}} >div</button>
+        <br/><br/>
+
+        
+        <input type="number" className="outStyle" value={this.state.total} />
+
+        <br/><br/>
+      </div>
+    )
+  }
 }
+
+
+
 
 export default App;
